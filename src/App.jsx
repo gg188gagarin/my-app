dddab47e9f3b5bc59bf86fd65713d7764b501aef
gg188gagarin <gg.188.gagarin@gmail.com>
2021-12-05 15:21:30 +0200

    verst3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import './App.css';
-import {Header} from "./components/Header";
-import {Main} from "./components/Main";
-import {Controls} from "./components/Controls";
+import { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+
+import { HomePage } from './pages/HomePage';
+import { Details } from './pages/Details';
+import { NotFound } from './pages/NotFound';
 
 function App() {
-  return (
-      <><Header/><Main>
-        <Controls></Controls>
-      </Main></>
-  );
+    const [countries, setCountries] = useState([]);
+
+    return (
+        <>
+            <Header />
+            <Main>
+                <Switch>
+                    <Route exact path="/">
+                        <HomePage countries={countries} setCountries={setCountries} />
+                    </Route>
+                    <Route path="/country/:name" component={Details} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Main>
+        </>
+    );
 }
 
 export default App;
